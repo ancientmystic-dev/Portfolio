ef7ef0ed4b14c53da86a57ec91a205309f89b3ea
ancientmystic-dev <kumarrgowda18@gmail.com>
2022-01-10 21:46:38 +0530

    twitter id

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -6,7 +6,7 @@ export const SEO: NextSeoProps = {
   titleTemplate: `${SITE_TITLE} | %s`,
   description: SITE_DESCRIPTION,
   twitter: {
-    handle: '@kumarrgowdaa',
+    handle: '@KumarGowda_',
     cardType: 'summary_large_image',
   },
   openGraph: {
