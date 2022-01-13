cb93db769cfd09e826a3f6e07bac5293cbe39f4b
ancientmystic-dev <kumarrgowda18@gmail.com>
2022-01-13 22:06:44 +0530

    Update MDImage.tsx

--- src/components/Markdown/MDImage.tsx
+++ src/components/Markdown/MDImage.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const MDImage: React.FC<Props> = ({ src, alt }) => (
   <Box my={3}>
-    <Zoom zoomMargin={200}>
+    <Zoom zoomMargin={40}>
       <Box
         sx={{
           position: 'relative',
